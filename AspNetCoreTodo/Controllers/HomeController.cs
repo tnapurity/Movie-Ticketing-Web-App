775ede2ffd70db9e7e052e776760b5b81c190ce7
tnapurity <tnapurity@gmail.com>
2020-10-09 13:05:46 -0600

    master

--- AspNetCoreTodo/Controllers/HomeController.cs
+++ AspNetCoreTodo/Controllers/HomeController.cs
@@ -20,7 +20,140 @@ namespace AspNetCoreTodo.Controllers
 
         public IActionResult Index()
         {
-            return View();
+            List<Movie> movieList = new List<Movie>();
+            movieList.Add(new Movie
+            {
+                Id = 1,
+                TotalCapacity = 150,
+                Title = "The Devil All the time",
+                imgUrl="/images/01.jpg",
+                PlotDsc="breif plot descriptions"
+                
+            });
+            movieList.Add(new Movie
+            {
+                Id = 2,
+                TotalCapacity = 150,
+                Title = "Knives Out",
+                imgUrl = "/images/02.jpg",
+                PlotDsc = "breif plot descriptions"
+            });
+            movieList.Add(new Movie
+            {
+                Id = 3,
+                TotalCapacity = 150,
+                Title = "Midsommar",
+                imgUrl = "/images/03.jpg",
+                PlotDsc = "breif plot descriptions"
+            });
+            movieList.Add(new Movie
+            {
+                Id = 4,
+                TotalCapacity = 150,
+                Title = "Tenet",
+                imgUrl = "/images/04.jpg",
+                PlotDsc = "breif plot descriptions"
+            });
+            movieList.Add(new Movie
+            {
+                Id = 5,
+                TotalCapacity = 150,
+                Title = "Tenet",
+                imgUrl = "/images/05.jpg",
+                PlotDsc = "breif plot descriptions"
+
+            });
+            movieList.Add(new Movie
+            {
+                Id = 6,
+                TotalCapacity = 150,
+                Title = "Inception",
+                imgUrl = "/images/06.jpg",
+                PlotDsc = "breif plot descriptions"
+            });
+            movieList.Add(new Movie
+            {
+                Id = 7,
+                TotalCapacity = 150,
+                Title = "Joker",
+                imgUrl = "/images/07.jpg",
+                PlotDsc = "breif plot descriptions"
+            });
+            movieList.Add(new Movie
+            {
+                Id = 8,
+                TotalCapacity = 150,
+                Title = "Zodiac",
+                imgUrl = "/images/08.jpg",
+                PlotDsc = "breif plot descriptions"
+            });
+
+            movieList.Add(new Movie
+            {
+                Id = 9,
+                TotalCapacity = 150,
+                Title = "The Boyz",
+                imgUrl = "/images/09.jpg",
+                PlotDsc = "breif plot descriptions"
+            });
+            movieList.Add(new Movie
+            {
+                Id = 10,
+                TotalCapacity = 150,
+                Title = "Raised by wolves",
+                imgUrl = "/images/10.jpg",
+                PlotDsc = "breif plot descriptions"
+            });
+            movieList.Add(new Movie
+            {
+                Id = 11,
+                TotalCapacity = 150,
+                Title = "Cobra Kai",
+                imgUrl = "/images/11.jpg",
+                PlotDsc = "breif plot descriptions"
+            });
+            movieList.Add(new Movie
+            {
+                Id = 12,
+                TotalCapacity = 150,
+                Title = "The Godfather",
+                imgUrl = "/images/12.jpg",
+                PlotDsc = "breif plot descriptions"
+            });
+            movieList.Add(new Movie
+            {
+                Id = 13,
+                TotalCapacity = 150,
+                Title = "After the colided",
+                imgUrl = "/images/13.jpg",
+                PlotDsc = "breif plot descriptions"
+            });
+            movieList.Add(new Movie
+            {
+                Id = 14,
+                TotalCapacity = 150,
+                Title = "Midway",
+                imgUrl = "/images/14.jpg",
+                PlotDsc = "breif plot descriptions"
+            });
+            movieList.Add(new Movie
+            {
+                Id = 15,
+                TotalCapacity = 150,
+                Title = "Interstellar",
+                imgUrl = "/images/15.jpg",
+                PlotDsc = "breif plot descriptions"
+            });
+            movieList.Add(new Movie
+            {
+                Id = 16,
+                TotalCapacity = 150,
+                Title = "The Dark Knight",
+                imgUrl = "/images/16.jpg",
+                PlotDsc = "breif plot descriptions"
+            });
+
+            return View(movieList);
         }
 
         public IActionResult Privacy()
---
+++ AspNetCoreTodo/Models/Movie.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AspNetCoreTodo.Models
+{
+    public class Movie
+    {
+
+        public int Id { get; set; }
+        public string Title  { get; set; }
+        public int TotalCapacity  { get; set; }
+        /// <summary>
+        /// Number of Booked 
+        /// </summary>
+        public int BookedCapacity { get; set; }
+        public string imgUrl { get; set; }
+        public string PlotDsc { get; set; }
+        
+        
+    }
+}
